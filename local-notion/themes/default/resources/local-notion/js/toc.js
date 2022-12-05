bb08ea11dd124713cae6c0f9a3037c577edd2055
Herman Schoenfeld <herman@sphere10.com>
2022-12-05 15:08:51 +1000

    LocalNotion theme updates

--- local-notion/themes/default/resources/local-notion/js/toc.js
+++ local-notion/themes/default/resources/local-notion/js/toc.js
@@ -1,14 +1,13 @@
-$(document).ready(function ($) {
+$(document).ready(function () {
     let heading = $(".ln-page-content").find(":header").not("[data-toc-skip]");
-
     for (let i = 0; i < heading.length; i++) {
         const head = heading[i];
-        const URL = head.innerText.replace(/ /g, '_')
-        $(head).attr('id', URL)
+        const URL = head.innerText.replace(/ /g, '_');
+        $(head).attr('id', URL);
         let header_level = parseInt(head.tagName.slice(-1));
         let spacing = "";
         spacing = `style="padding-left: ` + (header_level - 1) * 2 + `rem;"`
-        const this_head = `<li class="py-1"><a ${spacing} class="text-secondary" href="#${URL}">${head.innerText}</a></li>`
-        $('#toc').append(this_head)
+        const this_head = `<li class="py-1 ln-behaviour-hover-highlight" style="transform: rotate(0);"><a ${spacing} class="text-secondary stretched-link" href="#${URL}">${head.innerText}</a></li>`
+        $('.ln-toc').append(this_head)
     }
 });
