a78b7367a83d2ec682bfa9abb2df464d9b5a398b
Herman Schoenfeld <herman@sphere10.com>
2023-03-13 06:25:18 +1000

    Cosmetic changes to local notion themes

--- local-notion/themes/default/resources/local-notion/js/ln.js
+++ local-notion/themes/default/resources/local-notion/js/ln.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
 
     // remove empty block children
     $(".ln-block-children:not(:has(*))").remove();
-    
+    $(".ln-callout-children:not(:has(*))").remove();
 });
