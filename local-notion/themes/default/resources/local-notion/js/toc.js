f2e352b70a4e1e2cf4c3e3d33f63393e723a6646
Herman Schoenfeld <herman@sphere10.com>
2023-04-01 23:02:57 +1000

    Local Notion table of contents javascript bug

--- local-notion/themes/default/resources/local-notion/js/toc.js
+++ local-notion/themes/default/resources/local-notion/js/toc.js
@@ -2,12 +2,16 @@ $(document).ready(function () {
     let heading = $(".ln-page-content").find(":header").not("[data-toc-skip]");
     for (let i = 0; i < heading.length; i++) {
         const head = heading[i];
-        const URL = head.innerText.replace(/ /g, '_');
-        $(head).attr('id', URL);
+        const jhead = $(head);
+        var id = jhead.attr('id')
+        if (id === "") {
+            const URL = head.innerText.replace(/ /g, '_');
+            jhead.attr('id', URL);
+        }
         let header_level = parseInt(head.tagName.slice(-1));
         let spacing = "";
         spacing = `style="padding-left: ` + (header_level - 1) * 2 + `rem;"`
-        const this_head = `<li class="py-1 ln-behaviour-hover-highlight" style="transform: rotate(0);"><a ${spacing} class="text-secondary stretched-link" href="#${URL}">${head.innerText}</a></li>`
+        const this_head = `<li class="py-1 ln-behaviour-hover-highlight" style="transform: rotate(0);"><a ${spacing} class="text-secondary stretched-link" href="#${id}">${head.innerText}</a></li>`
         $('.ln-toc').append(this_head)
     }
 });
