6884fe5de3a56828cd41b0d5b28f6c1ee27c750f
Herman Schoenfeld <herman@sphere10.com>
2024-08-23 00:45:14 +1000

    Updated local notion themes

---
+++ local-notion/themes/cms/resources/js/cms_post.js
@@ -0,0 +1,20 @@
+﻿// Activate all typed text
+const toggles = document.querySelectorAll('[data-typed]');
+
+toggles.forEach((toggle) => {
+    const elementOptions = toggle.dataset.typed ? JSON.parse(toggle.dataset.typed) : {};
+
+    const defaultOptions = {
+        typeSpeed: 40,
+        backSpeed: 40,
+        backDelay: 1000,
+        loop: true,
+    };
+
+    const options = {
+        ...defaultOptions,
+        ...elementOptions,
+    };
+
+    new Typed(toggle, options);
+});
---
+++ local-notion/themes/cms/resources/js/cms_pre.js
@@ -0,0 +1,22 @@
+﻿function ReplaceParentAlignment(span) {
+    // Get the parent element (the p element)
+    var parentP = span.parentElement;
+
+    // List of alignment classes to check
+    var alignmentClasses = ['text-start', 'text-center', 'text-left', 'text-end'];
+
+    // Remove any existing alignment class on the parent
+    parentP.classList.forEach(function (className) {
+        if (alignmentClasses.includes(className)) {
+            parentP.classList.remove(className);
+        }
+    });
+
+    // Move the alignment class from span to parent p
+    span.classList.forEach(function (className) {
+        if (alignmentClasses.includes(className)) {
+            parentP.classList.add(className);
+            span.classList.remove(className);
+        }
+    });
+}
